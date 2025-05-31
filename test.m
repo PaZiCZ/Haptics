5bde0139be174cb0945fccaa02b8f44a3068c045
Jan-BredemeyerLeon <271486@vutbr.cz>
2025-05-31 05:15:18 +0200

    There was a mistake.

--- test.m
+++ test.m
@@ -1,6 +1,3 @@
-% Define the path where the data folders are located
-saveFolder = fullfile("C:\Users\Jan\Desktop\measurement");  % Replace with correct path
-
 % Create the 'Post-processing data' folder within saveFolder
 postProcessingFolder = fullfile(saveFolder, 'Post-processing data');
 if ~exist(postProcessingFolder, 'dir')
